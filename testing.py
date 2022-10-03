1200abb997c92689ae173fcb0d7c2dbe643e37c4
Zachariah Irwin <zachariah.irwin@colorado.edu>
2022-10-03 10:02:03 -0600

    Add plots (still have bugs)

--- testing.py
+++ testing.py
@@ -1,6 +1,8 @@
 import numpy as np
 import classElement
 
+import matplotlib.pyplot as plt
+
 class Parameters:
 
     def __init__(self):
@@ -12,7 +14,8 @@ class Parameters:
         self.rhoS_0 = 1e3
         self.rho_0 = self.ns*self.rhoS_0
 
-        self.grav = -9.81
+        # self.grav = -9.81
+        self.grav = 0
 
         self.numDOF   = 4
         self.numEl    = 2
@@ -39,7 +42,7 @@ coordinates[1,:,:] = np.array([[0.0,  0.0,  0.05],
                                [0.0,  0.01, 0.1]])
 
 LM       = np.ones((params.numElDOF, params.numEl), dtype=np.int32)
-LM      *= -2
+LM      *= -1
 # Set the free DOFs
 LM[1,1]  = 3
 LM[5,1]  = 0
@@ -49,18 +52,13 @@ LM[14,0] = 3
 LM[17,0] = 0
 LM[20,0] = 1
 LM[23,0] = 2
-# Set displacement BC DOFs
-LM[14,1] = -1
-LM[17,1] = -1
-LM[20,1] = -1
-LM[23,1] = -1
 
-params.g_displ = -0.05
+params.g_displ = -0.01
 
 params.TStart = 0.0
 params.TStop  = 1.0
 params.dt     = 1/50
-params.nsteps = np.round((params.TStop - params.TStart)/params.dt)
+params.nsteps = int(np.round((params.TStop - params.TStart)/params.dt))
 params.t      = params.TStart
 params.n      = 0
 
@@ -68,19 +66,26 @@ params.t_ramp = 1.0
 
 params.tolr = 1e-10
 params.tola = 1e-8
-params.kmax = 5
+params.kmax = 20
 
 D      = np.zeros((params.numDOF), dtype=np.float64)
 D_Last = np.zeros((params.numDOF), dtype=np.float64)
+
+DSolve = np.zeros((params.nsteps+1, params.numDOF), dtype=np.float64)
+isv_solve = np.zeros((params.nsteps+1,params.numEl,8,3,3,2), dtype=np.float64)
+stress_solve = np.zeros((params.nsteps+1,params.numEl,8,3,3,6), dtype=np.float64)
 
 gd_n = 0
 gd   = 0
 g    = np.zeros((params.numElDOF, params.numEl), dtype=np.float64)
 g_n  = np.zeros((params.numElDOF, params.numEl), dtype=np.float64)
 
+print("Solving...")
 while params.t < params.TStop:
 
     params.t += params.dt
+    params.n += 1
+    print("t = %.2f seconds" %params.t)
 
     gd_n      = gd
     g_n[14,1] = gd_n
@@ -106,7 +111,7 @@ while params.t < params.TStop:
     while Rtol > params.tolr and normR > params.tola:
 
         k += 1
-        if params.n == 0 and k == 1:
+        if params.n == 1 and k == 1:
             del_d = np.zeros((params.numDOF), dtype=np.float64)
         else:
             del_d = np.linalg.solve(dR, -R)
@@ -119,6 +124,9 @@ while params.t < params.TStop:
 
         for element_ID in range(params.numEl):
 
+            #------------------------
+            # Initialize the element.
+            #------------------------
             element = classElement.Element(a_GaussOrder=2, a_ID=element_ID)
             element.set_Gauss_Points()
             element.set_Gauss_Weights()
@@ -127,11 +135,53 @@ while params.t < params.TStop:
             element.get_Global_DOF(LM)
             element.set_Global_Solutions(D)
             element.apply_Local_BC(g)
-
+            #------------------------------
+            # Compute stresses and strains.
+            #------------------------------
             element.compute_variables(params)
+            #---------------------------
+            # Save stresses and strains.
+            #---------------------------
+            stress_solve[params.n,element.ID,:,:,:,0] = element.SPK
+            stress_solve[params.n,element.ID,:,:,:,1] = element.FPK
+            stress_solve[params.n,element.ID,:,:,:,2] = element.sigma
+            stress_solve[params.n,element.ID,:,:,:,3] = element.E
+            stress_solve[params.n,element.ID,:,:,:,4] = element.e
+            stress_solve[params.n,element.ID,:,:,:,5] = element.Hencky
 
+            # isv_solve[n,:,:,0,:] = np.trace(element.sigma)/3
+            # isv_solve[n,:,:,1,:] = np.sqrt(3/2)*np.linalg.norm(element.sigma - np.trace(element.sigma)/3*element.identity
+            #--------------------------------
+            # Compute internal force vectors.
+            #--------------------------------
             element.compute_forces(params)
+            #-----------------------------------
+            # Compute the consistent tangent(s).
+            #-----------------------------------
+            element.compute_tangents(params)
 
-            print(el.u_global)
+            for i in range(element.numDOF):
+                I = element.DOF[i]
 
-            print(el.ID)
+                if I > -1:
+                    R[I] += element.G_int[i]
+
+                    for j in range(element.numDOF):
+                        J = element.DOF[j]
+
+                        if J > -1:
+                            dR[I,J] += element.G_Mtx[i,j]
+
+        if k == 1:
+            R0 = R
+        Rtol = np.linalg.norm(R)/np.linalg.norm(R0)
+        normR = np.linalg.norm(R)
+
+        if k > params.kmax:
+            print(Rtol)
+            print(normR)
+            sys.exit("ERROR. Reached max number of iterations.")
+
+plt.figure(1)
+plt.plot(-stress_solve[:,0,0,2,2,3],-stress_solve[:,0,0,2,2,0]*1e-3)
+plt.show()
