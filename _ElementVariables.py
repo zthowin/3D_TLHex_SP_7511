b08ab81e46a9b44c4ba02535ddeea7d651133e89
Zachariah Irwin <zachariah.irwin@colorado.edu>
2022-10-03 10:01:32 -0600

    Add methods to compute stresses and strains

--- _ElementVariables.py
+++ _ElementVariables.py
@@ -31,6 +31,12 @@ def compute_variables(self, Parameters):
     self.get_C_inv()
     self.get_SPK(Parameters)
     self.get_FPK()
+    self.get_b()
+    self.get_v()
+    self.get_E()
+    self.get_e()
+    self.get_Hencky()
+    self.get_Cauchy()
     self.get_rho_0(Parameters)
     self.get_rho()
     return
@@ -66,7 +72,7 @@ def get_J(self):
 @register_method
 def get_C(self):
     # Compute right Cauchy-Green tensor.
-    self.C = np.einsum('...iI, ...iJ', self.F, self.F)
+    self.C = np.einsum('...iI, ...iJ', self.F, self.F, dtype=np.float64)
     return
 
 @register_method
@@ -79,24 +85,60 @@ def get_C_inv(self):
 def get_SPK(self, Parameters):
     # Compute second Piola-Kirchoff stress tensor.
     self.SPK = Parameters.mu*self.identity + np.einsum('..., ...IJ', Parameters.lambd*np.log(self.J) - Parameters.mu,\
-                                                                     self.C_inv)
+                                                                     self.C_inv, dtype=np.float64)
     return
 
 @register_method
 def get_FPK(self):
     # Compute first Piola-Kirchoff stress tensor.
-    self.FPK = np.einsum('...iI, ...JI', self.F, self.SPK)
+    self.FPK = np.einsum('...iI, ...JI', self.F, self.SPK, dtype=np.float64)
+    return
+
+@register_method
+def get_Cauchy(self):
+    # Compute Cauchy stress tensor.
+    self.sigma = np.einsum('...iI,...jI,...', self.FPK, self.F, 1/self.J, dtype=np.float64)
+    return
+
+@register_method
+def get_b(self):
+    # Compute left Cauchy-Green tensor.
+    self.b = np.einsum('...iI,...jI', self.F, self.F, dtype=np.float64)
+    return
+
+@register_method
+def get_v(self):
+    # Compute the left stretch tensor.
+    self.v = np.sqrt(self.b)
+    return
+
+@register_method
+def get_E(self):
+    # Compute Green-Lagrange strain tensor.
+    self.E = (self.C - self.identity)/2
+    return
+
+@register_method
+def get_e(self):
+    # Compute Euler-Almansi strain tensor.
+    self.e = (self.identity - np.linalg.inv(self.b))/2
+    return
+
+@register_method
+def get_Hencky(self):
+    # Compute Hencky strain.
+    self.Hencky = np.log(self.v)
     return
 
 @register_method
 def get_rho(self):
     # Compute mass density in current configuration.
-    self.rho = np.einsum('..., ...I', self.J, self.rho_0)
+    self.rho = np.einsum('..., ...I', self.J, self.rho_0, dtype=np.float64)
     return
 
 @register_method
 def get_rho_0(self, Parameters):
     # Compute mass density in reference configuration.
     # Should be constant for single-phase (i.e., classical continuum mechanics) materials.
-    self.rho_0 = Parameters.rhoS_0*np.ones((8,3))
+    self.rho_0 = Parameters.rhoS_0*np.ones((8,3), dtype=np.float64)
     return
