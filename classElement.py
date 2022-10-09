11fd12e8f3ff58a09757a711e24da51aff326b69
Zachariah Irwin <zachariah.irwin@colorado.edu>
2022-10-09 14:33:10 -0600

    Update docstrings

--- classElement.py
+++ classElement.py
@@ -3,7 +3,7 @@
 #
 # Author:       Zachariah Irwin
 # Institution:  University of Colorado Boulder
-# Last Edit:    October 4, 2022
+# Last Edit:    October 6, 2022
 #----------------------------------------------------------------------------------------
 import sys
 
@@ -37,23 +37,23 @@ except ImportError:
 class Element:
     
     def __init__(self, a_GaussOrder=None, a_ID=None):
-        # Set Gauss quadrature order.
+        # Initialize Gauss quadrature order.
         self.set_Gauss_Order(a_GaussOrder)
-        # Set element ID.
+        # Initialize element ID.
         self.set_Element_ID(a_ID)
         return
     
     def set_Gauss_Order(self, a_Order):
-        # Initialize the gauss quadrature order.
+        # Set the gauss quadrature order.
         self.Gauss_Order = a_Order
         return
 
     def set_Element_ID(self, a_ID):
-        # Initialize the element number.
+        # Set the element number.
         self.ID = a_ID
         return
 
-    def set_Gauss_Points(self):
+    def set_Gauss_Points(self, Parameters):
         # Initialize the Gauss quadrature points.
         if self.Gauss_Order == 2:
             const = 1/np.sqrt(3)
@@ -65,15 +65,15 @@ class Element:
                                      [const,  -const,  const],
                                      [const,   const,  const],
                                      [-const,  const,  const]],
-                                    dtype=np.float64)
+                                    dtype=Parameters.float_dtype)
         else:
             sys.exit("ERROR. Only trilinear elements have been implemented; check quadrature order.")
         return
 
-    def set_Gauss_Weights(self):
+    def set_Gauss_Weights(self, Parameters):
         # Initialize the Gauss quadrature weights.
         if self.Gauss_Order == 2:
-            self.weights = np.ones(8, dtype=np.float64)
+            self.weights = np.ones(8, dtype=Parameters.float_dtype)
         else:
             sys.exit("ERROR. Only trilinear elements have been implemented; check quadrature order.")
         return
@@ -83,7 +83,7 @@ class Element:
         self.coordinates = a_Coords
         return
 
-    def evaluate_Shape_Functions(self):
+    def evaluate_Shape_Functions(self, Parameters):
         # Initialize the shape functions used for interpolation.        
         #--------------------------------
         # Grab local element coordinates.
@@ -105,7 +105,7 @@ class Element:
         #-----------------------------
         # Build shape function matrix.
         #-----------------------------
-        self.Nu = np.zeros((8, 3, 24), dtype=np.float64)
+        self.Nu = np.zeros((8, 3, 24), dtype=Parameters.float_dtype)
         for i in range(3):
             self.Nu[:, i, 0 + i]  = self.N1
             self.Nu[:, i, 3 + i]  = self.N2
@@ -127,7 +127,7 @@ class Element:
         self.dN7_dxi = (1/8)*(1 + self.eta)*(1 + self.zeta)
         self.dN8_dxi = -self.dN7_dxi
         
-        self.dN_dxi      = np.zeros((8,8), dtype=np.float64)
+        self.dN_dxi      = np.zeros((8,8), dtype=Parameters.float_dtype)
         self.dN_dxi[:,0] = self.dN1_dxi
         self.dN_dxi[:,1] = self.dN2_dxi
         self.dN_dxi[:,2] = self.dN3_dxi
@@ -148,7 +148,7 @@ class Element:
         self.dN7_deta = -self.dN6_deta
         self.dN8_deta = -self.dN5_deta
         
-        self.dN_deta      = np.zeros((8,8), dtype=np.float64)
+        self.dN_deta      = np.zeros((8,8), dtype=Parameters.float_dtype)
         self.dN_deta[:,0] = self.dN1_deta
         self.dN_deta[:,1] = self.dN2_deta
         self.dN_deta[:,2] = self.dN3_deta
@@ -169,7 +169,7 @@ class Element:
         self.dN7_dzeta = -self.dN3_dzeta
         self.dN8_dzeta = -self.dN4_dzeta
         
-        self.dN_dzeta      = np.zeros((8,8), dtype=np.float64)
+        self.dN_dzeta      = np.zeros((8,8), dtype=Parameters.float_dtype)
         self.dN_dzeta[:,0] = self.dN1_dzeta
         self.dN_dzeta[:,1] = self.dN2_dzeta
         self.dN_dzeta[:,2] = self.dN3_dzeta
@@ -181,22 +181,22 @@ class Element:
         #-------------------
         # Compute jacobians.
         #-------------------
-        self.get_Jacobian()
+        self.get_Jacobian(Parameters)
         #----------------------------------
         # Compute shape function gradients.
         #----------------------------------
-        self.dN1_dx = np.einsum('...bI, ...b -> ...I', self.Jeinv, np.array([self.dN1_dxi, self.dN1_deta, self.dN1_dzeta]).T)
-        self.dN2_dx = np.einsum('...bI, ...b -> ...I', self.Jeinv, np.array([self.dN2_dxi, self.dN2_deta, self.dN2_dzeta]).T)
-        self.dN3_dx = np.einsum('...bI, ...b -> ...I', self.Jeinv, np.array([self.dN3_dxi, self.dN3_deta, self.dN3_dzeta]).T)
-        self.dN4_dx = np.einsum('...bI, ...b -> ...I', self.Jeinv, np.array([self.dN4_dxi, self.dN4_deta, self.dN4_dzeta]).T)
-        self.dN5_dx = np.einsum('...bI, ...b -> ...I', self.Jeinv, np.array([self.dN5_dxi, self.dN5_deta, self.dN5_dzeta]).T)
-        self.dN6_dx = np.einsum('...bI, ...b -> ...I', self.Jeinv, np.array([self.dN6_dxi, self.dN6_deta, self.dN6_dzeta]).T)
-        self.dN7_dx = np.einsum('...bI, ...b -> ...I', self.Jeinv, np.array([self.dN7_dxi, self.dN7_deta, self.dN7_dzeta]).T)
-        self.dN8_dx = np.einsum('...bI, ...b -> ...I', self.Jeinv, np.array([self.dN8_dxi, self.dN8_deta, self.dN8_dzeta]).T)
+        self.dN1_dx = np.einsum('...bI, ...b -> ...I', self.Jeinv, np.array([self.dN1_dxi, self.dN1_deta, self.dN1_dzeta]).T, dtype=Parameters.float_dtype)
+        self.dN2_dx = np.einsum('...bI, ...b -> ...I', self.Jeinv, np.array([self.dN2_dxi, self.dN2_deta, self.dN2_dzeta]).T, dtype=Parameters.float_dtype)
+        self.dN3_dx = np.einsum('...bI, ...b -> ...I', self.Jeinv, np.array([self.dN3_dxi, self.dN3_deta, self.dN3_dzeta]).T, dtype=Parameters.float_dtype)
+        self.dN4_dx = np.einsum('...bI, ...b -> ...I', self.Jeinv, np.array([self.dN4_dxi, self.dN4_deta, self.dN4_dzeta]).T, dtype=Parameters.float_dtype)
+        self.dN5_dx = np.einsum('...bI, ...b -> ...I', self.Jeinv, np.array([self.dN5_dxi, self.dN5_deta, self.dN5_dzeta]).T, dtype=Parameters.float_dtype)
+        self.dN6_dx = np.einsum('...bI, ...b -> ...I', self.Jeinv, np.array([self.dN6_dxi, self.dN6_deta, self.dN6_dzeta]).T, dtype=Parameters.float_dtype)
+        self.dN7_dx = np.einsum('...bI, ...b -> ...I', self.Jeinv, np.array([self.dN7_dxi, self.dN7_deta, self.dN7_dzeta]).T, dtype=Parameters.float_dtype)
+        self.dN8_dx = np.einsum('...bI, ...b -> ...I', self.Jeinv, np.array([self.dN8_dxi, self.dN8_deta, self.dN8_dzeta]).T, dtype=Parameters.float_dtype)
         #--------------------------------------
         # Construct strain-displacement matrix.
         #--------------------------------------
-        self.Bu = np.zeros((8, 9, 24), dtype=np.float64)
+        self.Bu = np.zeros((8, 9, 24), dtype=Parameters.float_dtype)
 
         for i in range(3):
             self.Bu[:, i, 0]  = self.dN1_dx[:,i]
@@ -229,66 +229,8 @@ class Element:
             self.Bu[:, i, 23] = self.dN8_dx[:,i-6]
         
         return
-    
-    def get_Jacobian(self):
-        # Compute the element Jacobian.
-        self.dx_dxi   = np.einsum('...i, i -> ...', self.dN_dxi,   self.coordinates[:,0])
-        self.dx_deta  = np.einsum('...i, i -> ...', self.dN_deta,  self.coordinates[:,0])
-        self.dx_dzeta = np.einsum('...i, i -> ...', self.dN_dzeta, self.coordinates[:,0])
-        
-        self.dy_dxi   = np.einsum('...i, i -> ...', self.dN_dxi,   self.coordinates[:,1])
-        self.dy_deta  = np.einsum('...i, i -> ...', self.dN_deta,  self.coordinates[:,1])
-        self.dy_dzeta = np.einsum('...i, i -> ...', self.dN_dzeta, self.coordinates[:,1])
-        
-        self.dz_dxi   = np.einsum('...i, i -> ...', self.dN_dxi,   self.coordinates[:,2])
-        self.dz_deta  = np.einsum('...i, i -> ...', self.dN_deta,  self.coordinates[:,2])
-        self.dz_dzeta = np.einsum('...i, i -> ...', self.dN_dzeta, self.coordinates[:,2])
-                
-        self.Je        = np.zeros((8,3,3),dtype=np.float64)
-        self.Je[:,0,0] = self.dx_dxi
-        self.Je[:,0,1] = self.dx_deta
-        self.Je[:,0,2] = self.dx_dzeta
-        self.Je[:,1,0] = self.dy_dxi
-        self.Je[:,1,1] = self.dy_deta
-        self.Je[:,1,2] = self.dy_dzeta
-        self.Je[:,2,0] = self.dz_dxi
-        self.Je[:,2,1] = self.dz_deta
-        self.Je[:,2,2] = self.dz_dzeta
-        
-        self.j     = np.zeros(8, dtype=np.float64)
-        self.Jeinv = np.zeros((8,3,3), dtype=np.float64)
 
-        for i in range(8):
-            self.j[i]          = np.linalg.det(self.Je[i,:,:])
-            self.Jeinv[i,:,:,] = np.linalg.inv(self.Je[i,:,:])
-        
-        return
-
-    def get_Global_DOF(self, a_LM):
-        # Set the global degrees of freedom of this element.
-        self.DOF    = a_LM[:,self.ID]
-        self.numDOF = self.DOF.shape[0]
-        return
-
-    def set_Global_Solutions(self, a_D):
-        # Set the local solution variables at the current time step.
-        self.set_u_global(a_D[self.DOF[0:24]])
-        return
-
-    def apply_Local_BC(self, a_g):
-        # Apply boundary conditions at the element scale.
-        if np.any(self.DOF < 0):
-            idxs = np.where((self.DOF == -1))[0]
-            for idx in idxs:
-                self.u_global[idx] = a_g[idx, self.ID]
-        return
-
-    def set_u_global(self, a_D):
-        # Initialize the global solid displacement (at element level).
-        self.u_global = a_D
-        return
-
-    def evaluate_Shape_Functions_2D(self):
+    def evaluate_Shape_Functions_2D(self, Parameters):
         # Create a 2D planar element for traction boundary condition.
         #---------
         # Set N_a.
@@ -300,7 +242,7 @@ class Element:
         #-----------------------------
         # Build shape function matrix.
         #-----------------------------
-        self.Nu_2D = np.zeros((4, 3, 24), dtype=np.float64)
+        self.Nu_2D = np.zeros((4, 3, 24), dtype=Parameters.float_dtype)
         for i in range(3):
             self.Nu_2D[:, i, 12 + i] = self.N5_2D
             self.Nu_2D[:, i, 15 + i] = self.N6_2D
@@ -314,7 +256,7 @@ class Element:
         self.dN7_dxi_2D = (1/4)*(1 + self.eta[4:8])
         self.dN8_dxi_2D = -self.dN7_dxi_2D
         
-        self.dN_dxi_2D      = np.zeros((4,4), dtype=np.float64)
+        self.dN_dxi_2D      = np.zeros((4,4), dtype=Parameters.float_dtype)
         self.dN_dxi_2D[:,0] = self.dN5_dxi_2D
         self.dN_dxi_2D[:,1] = self.dN6_dxi_2D
         self.dN_dxi_2D[:,2] = self.dN7_dxi_2D
@@ -327,7 +269,7 @@ class Element:
         self.dN7_deta_2D = -self.dN6_deta_2D
         self.dN8_deta_2D = -self.dN5_deta_2D
         
-        self.dN_deta_2D      = np.zeros((4,4), dtype=np.float64)
+        self.dN_deta_2D      = np.zeros((4,4), dtype=Parameters.float_dtype)
         self.dN_deta_2D[:,0] = self.dN5_deta_2D
         self.dN_deta_2D[:,1] = self.dN6_deta_2D
         self.dN_deta_2D[:,2] = self.dN7_deta_2D
@@ -335,27 +277,87 @@ class Element:
         #------------------------
         # Calculate the jacobian.
         #------------------------
-        self.get_Jacobian_2D()
+        self.get_Jacobian_2D(Parameters)
 
         return
-
-    def get_Jacobian_2D(self):
-        # Compute the 2D element Jacobian.
-        self.dx_dxi_2D  = np.einsum('ki, i -> k', self.dN_dxi_2D,   self.coordinates[4:8,0])
-        self.dx_deta_2D = np.einsum('ki, i -> k', self.dN_deta_2D,  self.coordinates[4:8,0])
+    
+    def get_Jacobian(self, Parameters):
+        # Compute the element Jacobian.
+        self.dx_dxi   = np.einsum('...i, i -> ...', self.dN_dxi,   self.coordinates[:,0], dtype=Parameters.float_dtype)
+        self.dx_deta  = np.einsum('...i, i -> ...', self.dN_deta,  self.coordinates[:,0], dtype=Parameters.float_dtype)
+        self.dx_dzeta = np.einsum('...i, i -> ...', self.dN_dzeta, self.coordinates[:,0], dtype=Parameters.float_dtype)
         
-        self.dy_dxi_2D  = np.einsum('ki, i -> k', self.dN_dxi_2D,   self.coordinates[4:8,1])
-        self.dy_deta_2D = np.einsum('ki, i -> k', self.dN_deta_2D,  self.coordinates[4:8,1])
+        self.dy_dxi   = np.einsum('...i, i -> ...', self.dN_dxi,   self.coordinates[:,1], dtype=Parameters.float_dtype)
+        self.dy_deta  = np.einsum('...i, i -> ...', self.dN_deta,  self.coordinates[:,1], dtype=Parameters.float_dtype)
+        self.dy_dzeta = np.einsum('...i, i -> ...', self.dN_dzeta, self.coordinates[:,1], dtype=Parameters.float_dtype)
+        
+        self.dz_dxi   = np.einsum('...i, i -> ...', self.dN_dxi,   self.coordinates[:,2], dtype=Parameters.float_dtype)
+        self.dz_deta  = np.einsum('...i, i -> ...', self.dN_deta,  self.coordinates[:,2], dtype=Parameters.float_dtype)
+        self.dz_dzeta = np.einsum('...i, i -> ...', self.dN_dzeta, self.coordinates[:,2], dtype=Parameters.float_dtype)
                 
-        self.Je_2D        = np.zeros((4,2,2),dtype=np.float64)
+        self.Je        = np.zeros((8,3,3), dtype=Parameters.float_dtype)
+        self.Je[:,0,0] = self.dx_dxi
+        self.Je[:,0,1] = self.dx_deta
+        self.Je[:,0,2] = self.dx_dzeta
+        self.Je[:,1,0] = self.dy_dxi
+        self.Je[:,1,1] = self.dy_deta
+        self.Je[:,1,2] = self.dy_dzeta
+        self.Je[:,2,0] = self.dz_dxi
+        self.Je[:,2,1] = self.dz_deta
+        self.Je[:,2,2] = self.dz_dzeta
+        
+        self.j     = np.zeros(8, dtype=Parameters.float_dtype)
+        self.Jeinv = np.zeros((8,3,3), dtype=Parameters.float_dtype)
+
+        for i in range(8):
+            self.j[i]          = np.linalg.det(self.Je[i,:,:])
+            self.Jeinv[i,:,:,] = np.linalg.inv(self.Je[i,:,:])
+        
+        return
+
+    def get_Jacobian_2D(self, Parameters):
+        # Compute the 2D element Jacobian.
+        self.dx_dxi_2D  = np.einsum('...i, i -> ...', self.dN_dxi_2D,   self.coordinates[4:8,0], dtype=Parameters.float_dtype)
+        self.dx_deta_2D = np.einsum('...i, i -> ...', self.dN_deta_2D,  self.coordinates[4:8,0], dtype=Parameters.float_dtype)
+        
+        self.dy_dxi_2D  = np.einsum('...i, i -> ...', self.dN_dxi_2D,   self.coordinates[4:8,1], dtype=Parameters.float_dtype)
+        self.dy_deta_2D = np.einsum('...i, i -> ...', self.dN_deta_2D,  self.coordinates[4:8,1], dtype=Parameters.float_dtype)
+                
+        self.Je_2D        = np.zeros((4,2,2), dtype=Parameters.float_dtype)
         self.Je_2D[:,0,0] = self.dx_dxi_2D
         self.Je_2D[:,0,1] = self.dx_deta_2D
         self.Je_2D[:,1,0] = self.dy_dxi_2D
         self.Je_2D[:,1,1] = self.dy_deta_2D
         
-        self.j_2D = np.zeros(4, dtype=np.float64)
+        self.j_2D = np.zeros(4, dtype=Parameters.float_dtype)
 
         for i in range(4):
             self.j_2D[i] = np.linalg.det(self.Je_2D[i,:,:])
         
+        return
+
+    def get_Global_DOF(self, a_LM):
+        # Set the global degrees of freedom of this element.
+        self.DOF    = a_LM[:,self.ID]
+        self.numDOF = self.DOF.shape[0]
+        return
+
+    def set_Global_Solutions(self, a_D):
+        # Set the local solution variables at the current time step.
+        # print(self.DOF.shape)
+        # print(a_D)
+        self.set_u_global(a_D[self.DOF[0:self.numDOF]])
+        return
+
+    def apply_Local_BC(self, a_g):
+        # Apply boundary conditions at the element scale.
+        if np.any(self.DOF < 0):
+            idxs = np.where((self.DOF == -1))[0]
+            for idx in idxs:
+                self.u_global[idx] = a_g[idx, self.ID]
+        return
+
+    def set_u_global(self, a_D):
+        # Initialize the global solid displacement (at element level).
+        self.u_global = a_D
         return
